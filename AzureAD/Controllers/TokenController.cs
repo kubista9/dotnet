f07a07288f6896b761f4261bace14ff2c9d7d19e
Jakub Kuka <jakubkuka07@gmail.com>
2024-08-05 14:02:08 +0200

    GetToken controller added

--- AzureAD/Controllers/TokenController.cs
+++ AzureAD/Controllers/TokenController.cs
@@ -1,6 +1,22 @@
+using AzureAD.Models;
+using AzureAD.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace AzureAD.Controllers;
 
-public class TokenController
+public class TokenController : ControllerBase
 {
-	
+	private readonly IAuth0Service _authService;
+
+	public TokenController(IAuth0Service auth0Service)
+	{
+		_authService = auth0Service;
+	}
+
+	[HttpGet]
+	public async Task<Token> GetToken()
+	{
+		var token = await _authService.GetTokenAsync();
+		return token;
+	}
 }
