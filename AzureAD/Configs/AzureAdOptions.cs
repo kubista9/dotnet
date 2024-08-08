9616c244b9359fdb7c549a1430f77ebe8e0d882e
Jakub Kuka <jakubkuka07@gmail.com>
2024-08-08 14:10:38 +0200

    Adding ? to nullable properties in AzureAdOptions

--- AzureAD/Configs/AzureAdOptions.cs
+++ AzureAD/Configs/AzureAdOptions.cs
@@ -2,10 +2,10 @@ namespace AzureAD.Configs;
 
 public class AzureAdOptions
 {
-	public string ClientId { get; set; }
-	public string ClientSecret { get; set; }
-	public string TenantId { get; set; }
-	public string Scope { get; set; }
+	public string? ClientId { get; set; }
+	public string? ClientSecret { get; set; }
+	public string? TenantId { get; set; }
+	public string? Scope { get; set; }
 	public string Authority => $"https://login.microsoftonline.com/{TenantId}";
 }
 
