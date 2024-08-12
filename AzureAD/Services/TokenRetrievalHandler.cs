0fe65565097607931798eaece4a85d8e9856d119
Jakub Kuka <jakubkuka07@gmail.com>
2024-08-12 21:29:05 +0200

    Making TokenretrievalHandler abstract and protected contstructor

--- AzureAD/Services/TokenRetrievalHandler.cs
+++ AzureAD/Services/TokenRetrievalHandler.cs
@@ -4,12 +4,13 @@ using AzureAD.Models;
 
 namespace AzureAD.Services;
 
-public class TokenRetrievalHandler : DelegatingHandler
+public abstract class TokenRetrievalHandler : DelegatingHandler
 {
 	private readonly ITokenService _tokenService;
 	private const string TokenRetrieval = nameof(TokenRetrieval);
 	private const string TokenKey = nameof(TokenKey);
-	public TokenRetrievalHandler(ITokenService tokenService)
+
+	protected TokenRetrievalHandler(ITokenService tokenService)
 	{
 		_tokenService = tokenService;
 	}
