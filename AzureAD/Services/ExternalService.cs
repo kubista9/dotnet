cfb7a124d061a2636339a657de5c2cf1b238f138
Jakub Kuka <jakubkuka07@gmail.com>
2024-08-13 16:02:34 +0200

    Polly added to ExternalService

--- AzureAD/Services/ExternalService.cs
+++ AzureAD/Services/ExternalService.cs
@@ -6,12 +6,12 @@ namespace AzureAD.Services;
 
 public class ExternalService<T> : IExternalService<T>
 {
-	private readonly HttpClient _httpClient;
+	//private readonly HttpClient _httpClient;
 	private readonly TokenRetrievalHandler _tokenRetrievalHandler;
 
-	public ExternalService(HttpClient httpClient, TokenRetrievalHandler tokenRetrievalHandler)
+	public ExternalService(TokenRetrievalHandler tokenRetrievalHandler)
 	{
-		_httpClient = httpClient;
+		//_httpClient = httpClient;
 		_tokenRetrievalHandler = tokenRetrievalHandler;
 	}
 	public async Task<T?> GetAsync(string endpoint, Dictionary<string, string>? queryParameters)
@@ -29,12 +29,13 @@ public class ExternalService<T> : IExternalService<T>
 		}
 
 		var request = new HttpRequestMessage(HttpMethod.Get, builder.Uri);
-		var cancellationToken = new CancellationToken();
 
-		var response = await _tokenRetrievalHandler.SendAsync(request, cancellationToken);
+		var response = await PollyPolicies.RetryPolicy.ExecuteAsync(async () =>
+			await _tokenRetrievalHandler.SendAsync(request, CancellationToken.None));
 		response.EnsureSuccessStatusCode();
-		var content = await response.Content.ReadAsStringAsync(cancellationToken);
 
+		var cancellationToken = new CancellationToken();
+		var content = await response.Content.ReadAsStringAsync(cancellationToken);
 		return JsonSerializer.Deserialize<T>(content);
 	}
 }
